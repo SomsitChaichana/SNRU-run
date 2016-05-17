63939a0ee265094a4ee0c10a9655d7722b538248
somsit <c_somsit@hotmail.com>
2016-05-17 09:42:15 +0700

    17/5/59

--- app/src/main/java/snru/somsit/chaichanat/snrurun/SingUp.java
+++ app/src/main/java/snru/somsit/chaichanat/snrurun/SingUp.java
@@ -14,7 +14,7 @@ public class SingUp extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton,
             choice4RadioButton, choice5RadioButton;
-    private String nameString, userString, passwordString, avataString;
+    private String nameString, userString, passwordString, avataString = "0";
 
     @Override
 
@@ -25,7 +25,37 @@ public class SingUp extends AppCompatActivity {
         //Blind Widget
         blindWidget();
 
+        //Radio Button Con
+        radioButtonController();
+
     }//main method
+
+    private void radioButtonController() {
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override//ตรวจสอบเลือกรูป
+            public void onCheckedChanged(RadioGroup group, int i) {
+                switch (i) {
+                    case R.id.radioButton:
+                        avataString = "0";
+                        break;
+                    case R.id.radioButton2:
+                        avataString = "1";
+                        break;
+                    case R.id.radioButton3:
+                        avataString = "2";
+                        break;
+                    case R.id.radioButton4:
+                        avataString = "3";
+                        break;
+                    case R.id.radioButton5:
+                        avataString = "4";
+                        break;
+                }
+            }
+        });
+
+    }
 
     private void blindWidget() {
         nameEditText = (EditText) findViewById(R.id.editText);
@@ -54,6 +84,7 @@ public class SingUp extends AppCompatActivity {
             myAlert.myDialog(this,"มีช่องว่าง", "กรุณากรอกทุกช่อง");
 
         } else {
+
         }
 
     }
